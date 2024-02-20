32e0201233c71f82fb774a628a0d03cf5556f18a
Filip Madzia <filipmadzia33@gmail.com>
2024-02-20 19:59:17 +0100

    Zadania przed kartkówką

---
+++ csv-file-exercise/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace csv_file_exercise;
+
+public class Hotel
+{
+	[Index(0)]
+	public int Id { get; set; }
+	[Index(1)]
+	public string Name { get; set; }
+	[Index(2)]
+	public string PhoneNubmer { get; set; }
+	[Index(3)]
+	public string Email { get; set; }
+	[Index(4)]
+	public string ServiceType { get; set; }
+	[Index(5)]
+	public string FacilityCategory { get; set; }
+	[Index(6)]
+	public string FacilityType { get; set; }
+	[Index(7)]
+	public string Address { get; set; }
+}
--- csv-file-exercise/Program.cs
+++ csv-file-exercise/Program.cs
@@ -8,6 +8,53 @@ class Program
 {
 	static void Main()
 	{
+		HotelsExercise();
+	}
+
+	static void HotelsExercise()
+	{
+		var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+		{
+			Delimiter = ";"
+		};
+
+		using var reader = new StreamReader("hotele.csv");
+		using var csv = new CsvReader(reader, config);
+
+		var hotels = csv.GetRecords<Hotel>().ToList();
+
+        // zad 1
+        Console.WriteLine("===Zad 1===");
+        var zad1 = hotels.Where(x => x.Address.Contains("Bielsko-Bia"));
+		foreach(var hotel in zad1)
+		{
+			Console.WriteLine($"{hotel.Name} - {hotel.Address}");
+		}
+
+		// zad 2
+        Console.WriteLine("===Zad 2===");
+		var zad2 = hotels.Where(x => x.FacilityCategory.Contains("****")).Count();
+        Console.WriteLine(zad2);
+
+        // zad 3
+        Console.WriteLine("===Zad 3===");
+		var zad3 = hotels.DistinctBy(x => x.FacilityCategory);
+		foreach(var hotel in zad3)
+		{
+            Console.WriteLine(hotel.FacilityCategory);
+        }
+
+        // zad 4
+        Console.WriteLine("===Zad 4===");
+		var zad4 = hotels.DistinctBy(x => x.ServiceType);
+		foreach(var hotel in zad4)
+		{
+            Console.WriteLine(hotel.ServiceType);
+        }
+    }
+
+	static void Test()
+	{
 		var config = new CsvConfiguration(CultureInfo.InvariantCulture);
 
 		using var reader = new StreamReader("users_data.csv");
@@ -25,7 +72,7 @@ class Program
 			}
 		}
 
-        Console.WriteLine($"Pokemon count: {pokemonCount}");
+		Console.WriteLine($"Pokemon count: {pokemonCount}");
 
 		var disctinctPokemons = records.Select(x => x.Gender).Where(x => x != "Male" && x != "Female").Distinct();
 
