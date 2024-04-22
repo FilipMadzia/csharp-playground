b6118f2caa6dbce769a91401eb1e79feb87494e8
Filip Madzia <filipmadzia33@gmail.com>
2024-04-22 21:25:04 +0200

    operatro overloading

---
+++ operator-overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace operator_overloading;
+
+class Program
+{
+	static void Main()
+	{
+		var tensor1 = new Tensor(new double[,]
+		{
+			{ 0, 1, 2 },
+			{ 0, 1, 2 },
+			{ 0, 1, 2 }
+		});
+		var tensor2 = new Tensor(new double[,]
+		{
+			{ 3, 1, 2 },
+			{ 3, 1, 2 },
+			{ 3, 1, 2 }
+		});
+
+		tensor1.Display();
+		tensor2.Display();
+
+		var tensor3 = tensor1 + tensor2;
+
+		tensor3.Display();
+	}
+}
---
+++ operator-overloading/Tensor.cs
@@ -0,0 +1,34 @@
+﻿namespace operator_overloading;
+
+class Tensor(double[,] tab)
+{
+	private readonly double[,] tab = tab;
+
+	public static Tensor operator +(Tensor left, Tensor right)
+	{
+		var tensor = new double[3,3];
+
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				tensor[i, j] = left.tab[i, j] + right.tab[i, j];
+			}
+		}
+
+		return new Tensor(tensor);
+	}
+
+	public void Display()
+	{
+		for(int i = 0; i < tab.GetLength(0); i++)
+		{
+			for(int j = 0; j < tab.GetLength(1); j++)
+			{
+				Console.Write(tab[i, j]);
+			}
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
