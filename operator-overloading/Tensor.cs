3071327b3f551f15e08978997c8b168166a88fe7
Filip Madzia <filipmadzia33@gmail.com>
2024-04-23 22:28:01 +0200

    Tensor

--- operator-overloading/Tensor.cs
+++ operator-overloading/Tensor.cs
@@ -19,6 +19,34 @@ class Tensor(double[,] tab)
 		return new Tensor(tensor);
 	}
 
+	public static Tensor operator *(Tensor left, double k)
+	{
+		var tensor = new double[3, 3];
+
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++)
+			{
+				tensor[i, j] = left.tab[i, j] * k;
+			}
+		}
+
+		return new Tensor(tensor);
+	}
+
+	public double this[int i, int j]
+	{
+		get
+		{
+			if(i < 0 || i > 2 || j < 0 || j > 2)
+			{
+				throw new Exception();
+			}
+
+			return tab[i, j];
+		}
+	}
+
 	public void Display()
 	{
 		for(int i = 0; i < tab.GetLength(0); i++)
