315b4fcc61f2c71bdce728ea40df2fb516f1ace9
Filip Madzia <filipmadzia33@gmail.com>
2024-09-26 19:19:04 +0200

    Exploring attributes

---
+++ exploring-attributes/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exploring_attributes;
+
+class Person
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	[Uppercase]
+	public string NormalizedLastName { get; set; }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var peter = new Person();
+		peter.NormalizedLastName = "peter";
+
+		Console.WriteLine(peter.NormalizedLastName);
+	}
+}
