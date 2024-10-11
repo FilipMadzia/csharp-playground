4084013b62c2a02c44794067c16d2ed5e8f86f7c
Filip Madzia <filipmadzia33@gmail.com>
2024-10-11 21:05:04 +0200

    Bubble sort benchmark - unoptimized bubble sort

---
+++ bubble-sort-benchmark/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace bubble_sort_benchmark;
+
+class Program
+{
+	static void BubbleSortUnoptimized(List<int> x)
+	{
+		for(var i = 0; i < x.Count; i++)
+		{
+			for(var j = 0; j < x.Count - 1; j++)
+			{
+				if(x[j] > x[j + 1])
+				{
+					var temp = x[j];
+					x[j] = x[j + 1];
+					x[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	static void Main()
+	{
+		var random = new Random();
+
+		var unsortedList = Enumerable.Range(0, 10000).Select(x => random.Next(0, 100));
+
+		var startTime = DateTime.Now;
+
+		BubbleSortUnoptimized(unsortedList.ToList());
+
+		var endTime = DateTime.Now;
+
+		Console.WriteLine($"Unsorted bubble sort time: {(endTime - startTime).TotalSeconds}s");
+	}
+}
