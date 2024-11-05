da3d8fbd7509d0024189006569a6fdcee45b7b2d
Filip Madzia <filipmadzia33@gmail.com>
2024-11-05 20:45:56 +0100

    Visualizing bubble sort algorithm

--- list-visualizer/MainWindow.xaml.cs
+++ list-visualizer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -7,6 +8,7 @@ namespace list_visualizer;
 public partial class MainWindow
 {
 	List<int> Data { get; set; }
+	int BarGap { get; set; } = 2;
 
 	public MainWindow()
 	{
@@ -15,6 +17,42 @@ public partial class MainWindow
 		Data = FillData();
 
 		DrawList(Data);
+	}
+	
+	private void RestartButtonOnClick(object sender, RoutedEventArgs e)
+	{
+		Data = FillData();
+		
+		DrawList(Data);
+	}
+	
+	private void BubbleSortButtonOnClick(object sender, RoutedEventArgs e)
+	{
+		BubbleSort(Data);
+	}
+	
+	async void BubbleSort(List<int> list)
+	{
+		for(int i = 0; i < list.Count; i++)
+		{
+			bool swapped = false;
+
+			for(int j = 0; j < list.Count - i - 1; j++)
+			{
+				if(list[j] > list[j + 1])
+				{
+					(list[j], list[j + 1]) = (list[j + 1], list[j]);
+
+					swapped = true;
+
+					DrawList(list);
+					
+					await Task.Delay(1);
+				}
+			}
+
+			if(!swapped) break;
+		}
 	}
 
 	List<int> FillData()
@@ -28,27 +66,25 @@ public partial class MainWindow
 
 	void DrawList(List<int> list)
 	{
-		var barWidth = canvas.Width / list.Count;
-		var index = 0;
+		Canvas.Children.Clear();
+		
+		var barWidth = (Canvas.Width - BarGap * list.Count) / list.Count;
 
-		foreach(var item in list)
+		for (var i = 0; i < list.Count; i++)
 		{
-			var barHeight = item * (canvas.Height / 100);
-
+			var barHeight = list[i] * (Canvas.Height / 100);
+			
 			var bar = new Rectangle
 			{
 				Width = (int)barWidth,
 				Height = (int)barHeight,
 				Fill = Brushes.Blue
 			};
-
-			Canvas.SetLeft(bar, index * barWidth);
+			
+			Canvas.SetLeft(bar, i * (barWidth + BarGap));
 			Canvas.SetBottom(bar, 0);
 
-
-			canvas.Children.Add(bar);
-
-			index++;
+			Canvas.Children.Add(bar);
 		}
 	}
 }
