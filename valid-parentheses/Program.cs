6878a51200d68c173eb17f644611d0b7dc8b8d7a
Filip Madzia <filipmadzia33@gmail.com>
2024-11-12 21:56:56 +0100

    Valid parentheses, a LeetCode problem

---
+++ valid-parentheses/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace valid_parentheses;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine(ValidParentheses("(){}[]"));
+		Console.WriteLine(ValidParentheses("({[]})"));
+		Console.WriteLine(ValidParentheses("(}[({"));
+	}
+
+	static bool ValidParentheses(string s)
+	{
+		var stack = new Stack<char>();
+
+		foreach (var c in s)
+		{
+			switch(c)
+			{
+				case '(':
+					stack.Push(c);
+					break;
+				case ')':
+					if (stack.Count == 0)
+						return false;
+					if (stack.Peek() == '(')
+					{
+						stack.Pop();
+					}
+					else
+					{
+						stack.Push(c);
+					}
+                    
+					break;
+				case '{':
+					stack.Push(c);
+					break;
+				case '}':
+					if (stack.Count == 0)
+						return false;
+					if (stack.Peek() == '{')
+					{
+						stack.Pop();
+					}
+					else
+					{
+						stack.Push(c);
+					}
+                    
+					break;
+				case '[':
+					stack.Push(c);
+					break;
+				case ']':
+					if (stack.Count == 0)
+						return false;
+					if (stack.Peek() == '[')
+					{
+						stack.Pop();
+					}
+					else
+					{
+						stack.Push(c);
+					}
+                    
+					break;
+				default:
+					throw new Exception();
+					break;
+			}
+		}
+
+		return stack.Count == 0;
+	}
+}
