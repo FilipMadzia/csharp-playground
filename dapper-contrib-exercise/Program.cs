67d7799fa95c86055c7b7eb4fba8dbbf49823f8a
Filip Madzia <filipmadzia33@gmail.com>
2024-11-17 20:37:26 +0100

    Dapper Contrib exercise

--- dapper-contrib-exercise/Program.cs
+++ dapper-contrib-exercise/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace dapper_contrib_exercise;
+﻿using System.Data.SqlClient;
+using dapper_contrib_exercise.Entities;
+using Dapper.Contrib.Extensions;
+
+namespace dapper_contrib_exercise;
 
 class Program
 {
     static void Main()
     {
-		Console.WriteLine("Hello, World!");
-	}
+	    const string connectionString = "Database=HotelixDb; Trusted_Connection=true";
+
+	    using (var connection = new SqlConnection(connectionString))
+	    {
+		    connection.Open();
+
+		    var hotels = connection.GetAll<HotelEntity>().ToList();
+		    
+		    hotels.ForEach(Console.WriteLine);
+	    }
+    }
 }
