06469c6d831f715b25be4a3651c262da9ed2368d
Filip Madzia <filipmadzia33@gmail.com>
2024-11-17 20:20:48 +0100

    ado.net exercise

---
+++ ado-dotnet-exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+static class Program
+{
+	static void Main()
+	{
+		const string connectionString = "Initial Catalog=HotelixDb; Integrated Security=true";
+		const string sqlQuery = "SELECT * FROM dbo.Cities WHERE Name = @Name";
+
+		using var connection = new SqlConnection(connectionString);
+		
+		var command = new SqlCommand(sqlQuery, connection);
+		command.Parameters.AddWithValue("@Name", "Bielsko-Biała");
+
+		try
+		{
+			connection.Open();
+
+			var reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				Console.WriteLine($"{reader[0]}, {reader[1]}");
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+	}
+}
