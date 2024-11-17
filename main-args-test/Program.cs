1352051286b855b4db87ed40acabce13fb2505d0
Filip Madzia <filipmadzia33@gmail.com>
2024-11-17 21:45:32 +0100

    More tests with Main() args

--- main-args-test/Program.cs
+++ main-args-test/Program.cs
@@ -4,9 +4,35 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		foreach (var arg in args)
+		if (args.Length != 1)
+			throw new ArgumentException("Wrong number of arguments");
+		
+		var availableArgs = new List<string> { "-development", "-production" };
+
+		if (!availableArgs.Contains(args[0]))
+			throw new ArgumentException("Invalid argument");
+
+
+		switch (args[0])
 		{
-			Console.WriteLine(arg);
+			case "-development":
+				Development();
+				break;
+			case "-production":
+				Production();
+				break;
+			default:
+				throw new ArgumentException("Invalid argument");
 		}
+	}
+	
+	static void Development()
+	{
+		Console.WriteLine("Launched in development mode");
+	}
+
+	static void Production()
+	{
+		Console.WriteLine("Launched in production mode");
 	}
 }
