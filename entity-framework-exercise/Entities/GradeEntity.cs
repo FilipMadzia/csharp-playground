b6a917002a489ea42d9ed0ea7d3fafafb84c9225
FilipMadzia <filipmadzia33@gmail.com>
2025-01-28 17:31:47 +0100

    Remove old projects

--- entity-framework-exercise/Entities/GradeEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace entity_framework_exercise.Entities;
-
-public class GradeEntity
-{
-    public int Id { get; set; }
-    public string GradeName { get; set; } = string.Empty;
-
-    public List<StudentEntity> Students { get; set; } = [];
-}
