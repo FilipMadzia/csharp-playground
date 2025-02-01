87024580a7409ab445798a44e2f87775b82916d2
Filip Madzia <filipmadzia33@gmail.com>
2025-02-01 16:24:01 +0100

    Basic game

--- boat-game/MainPage.xaml.cs
+++ boat-game/MainPage.xaml.cs
@@ -2,22 +2,72 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+	private double _rotation = 0;
+	private double _speed = 3;
+	private double _rotationSpeed = 3;
+	private double _x = 0;
+	private double _y = 0;
+	private double _refreshRate = 30;
 
-    public MainPage()
-    {
-        InitializeComponent();
-    }
+	private IDispatcherTimer _timer;
+	private IDispatcherTimer _turnTimer;
 
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
+	public MainPage()
+	{
+		InitializeComponent();
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+		_timer = Dispatcher.CreateTimer();
+		_timer.Interval = TimeSpan.FromMilliseconds(_refreshRate);
+		_timer.Tick += OnTimerTick;
+		_timer.Start();
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
-    }
+		_turnTimer = Dispatcher.CreateTimer();
+		_turnTimer.Interval = TimeSpan.FromMilliseconds(_refreshRate);
+	}
+
+	private void OnTurnLeftButtonPressed(object sender, EventArgs e)
+	{
+		_turnTimer.Tick += OnTurnLeft;
+		_turnTimer.Start();
+	}
+
+	private void OnTurnLeftButtonReleased(object sender, EventArgs e)
+	{
+		_turnTimer.Tick -= OnTurnLeft;
+		_turnTimer.Stop();
+	}
+
+	private void OnTurnRightButtonPressed(object sender, EventArgs e)
+	{
+		_turnTimer.Tick += OnTurnRight;
+		_turnTimer.Start();
+	}
+
+	private void OnTurnRightButtonReleased(object sender, EventArgs e)
+	{
+		_turnTimer.Tick -= OnTurnRight;
+		_turnTimer.Stop();
+	}
+
+	private void OnTurnLeft(object sender, EventArgs e)
+	{
+		_rotation -= _rotationSpeed;
+		BoatImage.Rotation = _rotation;
+	}
+
+	private void OnTurnRight(object sender, EventArgs e)
+	{
+		_rotation += _rotationSpeed;
+		BoatImage.Rotation = _rotation;
+	}
+
+	private void OnTimerTick(object sender, EventArgs e)
+	{
+		double radians = _rotation * Math.PI / 180;
+		_x += _speed * Math.Cos(radians);
+		_y += _speed * Math.Sin(radians);
+
+		BoatImage.TranslationX = _x;
+		BoatImage.TranslationY = _y;
+	}
 }
