f686d6deb7d9a42d296b1d1ed8d3589d2cef8800
Filip Madzia <filipmadzia33@gmail.com>
2025-03-12 16:22:49 +0100

    Remove old projects

--- many-to-many/Models/Author.cs
+++
@@ -1,9 +0,0 @@
-namespace many_to_many.Models;
-
-public class Author
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public ICollection<Book> Books { get; set; } = [];
-}
--- many-to-many/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace many_to_many.Models;
-
-public class Book
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public ICollection<Author> Authors { get; set; } = [];
-}
