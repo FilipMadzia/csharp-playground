5e90cccf7fc6d17b72262d2be795f3d820de1271
FilipMadzia <filipmadzia33@gmail.com>
2025-03-17 16:24:04 +0100

    calculator

--- wpf-application/MainWindow.xaml.cs
+++ wpf-application/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace wpf_application;
 
@@ -16,7 +9,9 @@ namespace wpf_application;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private string equation = string.Empty;
+    private string? _firstEquation;
+    private string? _operation;
+    private string? _secondEquation;
 
     public MainWindow()
     {
@@ -32,14 +27,68 @@ public partial class MainWindow : Window
         
         var content = button.Content;
         
-        if ((string)ResultLbl.Content == "0")
-            ResultLbl.Content = button.Content.ToString();
+        if (_firstEquation == null && _secondEquation == null)
+            _firstEquation = content.ToString();
+        else if(_firstEquation != null && _secondEquation == null && _operation == null)
+            _firstEquation += content.ToString();
+        else if(_firstEquation != null && _secondEquation == null && _operation != null)
+            _secondEquation = content.ToString();
         else
-            ResultLbl.Content += button.Content.ToString();
+            _secondEquation += content.ToString();
+        
+        if ((string)ResultLbl.Content == "0")
+            ResultLbl.Content = content.ToString();
+        else
+            ResultLbl.Content += content.ToString();
     }
 
     private void ClearButtonClick(object sender, RoutedEventArgs e)
     {
+        _operation = null;
+        _secondEquation = null;
         ResultLbl.Content = "0";
+    }
+
+    private void ParseEquation(object sender, RoutedEventArgs e)
+    {
+        var num1 = double.Parse(_firstEquation);
+        var num2 = double.Parse(_secondEquation);
+
+        var result = 0d;
+
+        switch (_operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+
+        ResultLbl.Content = result.ToString();
+        _firstEquation = result.ToString();
+        _secondEquation = null;
+        _operation = null;
+    }
+
+    private void MathAction(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+        
+        if (button == null)
+            throw new ArgumentException("Wrong type");
+        
+        var content = button.Content;
+        
+        _operation = content.ToString();
+        
+        ResultLbl.Content += _operation;
     }
 }
