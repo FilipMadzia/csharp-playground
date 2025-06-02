bd3d8a8f4df27786de353d709d8fb79b6c4bae46
FilipMadzia <filipmadzia33@gmail.com>
2025-06-02 16:26:34 +0200

    todolist

---
+++ School2425/TodoListApp/Models/TodoListItem.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoListApp.Models;
+
+public class TodoListItem
+{
+	public string Title { get; set; } = string.Empty;
+	public bool IsCompleted { get; set; }
+}
---
+++ School2425/TodoListApp/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using TodoListApp.Models;
+
+namespace TodoListApp.ViewModels;
+
+public class TodoListViewModel
+{
+	public ObservableCollection<TodoListItem> TodoList { get; set; } = [
+		new() { Title = "Zadanie fizyka", IsCompleted = false },
+		new() { Title = "Zakupy", IsCompleted = false },
+		new() { Title = "Projekt programowanie", IsCompleted = true } ];
+}
