0b7ec80e229f5166f1eb9d746329c8bd4625fb05
Filip Madzia <filipmadzia33@gmail.com>
2025-06-11 22:59:57 +0200

    Update Program.cs

--- InvincibleBrainrotAudioPlayer/Program.cs
+++ InvincibleBrainrotAudioPlayer/Program.cs
@@ -1,44 +1,52 @@
-﻿using NAudio.Wave;
+using NAudio.Wave;
 
 class Program
 {
-	static WaveOutEvent waveOutDevice = new();
-	static AudioFileReader audioFileReader = new("Audios/a-steak.mp3");
-	static string[] audioFilePaths = [
-		"a-steak.mp3",
-		"i-made-a-steak.mp3",
-		"invincible-pretty-sure.mp3",
-		"omni-man-are-you-sure.mp3",
-		"sea-salt.mp3",
-		"stand-ready-for-my-arrival-worm.mp3",
-		"where-is-omni-man.mp3"
-	];
-	static int intervalInSeconds;
+    static WaveOutEvent waveOutDevice = new();
+    static AudioFileReader audioFileReader;
+    static readonly string[] audioFilePaths =
+    {
+        "a-steak.mp3",
+        "i-made-a-steak.mp3",
+        "invincible-pretty-sure.mp3",
+        "omni-man-are-you-sure.mp3",
+        "sea-salt.mp3",
+        "stand-ready-for-my-arrival-worm.mp3",
+        "where-is-omni-man.mp3"
+    };
+    static int intervalInSeconds;
 
-	static void Main(string[] args)
-	{
-		intervalInSeconds = args.Length > 0 ? int.Parse(args[0]) : 5;
+    static void Main(string[] args)
+    {
+        intervalInSeconds = args.Length > 0 ? int.Parse(args[0]) : 5;
+        waveOutDevice = new WaveOutEvent();
+        InitializeAudio();
 
-		while (true)
-		{
-			var audioFilePath = GetRandomAudioFilePath();
-			SetAudioFile(audioFilePath);
+        while (true)
+        {
+            var audioFilePath = GetRandomAudioFilePath();
+            SetAudioFile(audioFilePath);
 
-			waveOutDevice.Play();
+            waveOutDevice.Play();
 
-			Thread.Sleep(intervalInSeconds * 1000);
-		}
-	}
+            Thread.Sleep(intervalInSeconds * 1000);
+        }
+    }
 
-	static string GetRandomAudioFilePath() => audioFilePaths[Random.Shared.Next(audioFilePaths.Length)];
+    static void InitializeAudio()
+    {
+        audioFileReader = new AudioFileReader($"Audios/{audioFilePaths[0]}");
+        waveOutDevice.Init(audioFileReader);
+    }
 
-	static void SetAudioFile(string fileName)
-	{
-		waveOutDevice.Stop();
+    static string GetRandomAudioFilePath() => audioFilePaths[Random.Shared.Next(audioFilePaths.Length)];
 
-		audioFileReader?.Dispose();
-		
-		audioFileReader = new AudioFileReader($"Audios/{fileName}");
-		waveOutDevice.Init(audioFileReader);
-	}
+    static void SetAudioFile(string fileName)
+    {
+        waveOutDevice.Stop();
+        audioFileReader.Dispose();
+
+        audioFileReader = new AudioFileReader($"Audios/{fileName}");
+        waveOutDevice.Init(audioFileReader);
+    }
 }
